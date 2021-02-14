9079f330c05b2787ace2591b9ea913502fba5547
Roberto Hanus <robertohanus@hotmail.com>
2021-02-14 16:00:32 -0300

    Implemented.

--- src/atr2atc/Main.java
+++ src/atr2atc/Main.java
@@ -6,6 +6,10 @@
 package atr2atc;
 
 import java.io.Console;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  *
@@ -18,7 +22,38 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println(args[0]);
+
+        byte[] header = new byte[16];
+        byte[] sectorInput = new byte[128];
+        byte[] sectorOutputC = new byte[129];
+        byte checkSum = 0x00;
+
+        try {
+        InputStream inputStream = new FileInputStream(args[0]);
+        OutputStream outputStream = new FileOutputStream(args[1]);
+
+        
+            inputStream.read(header);
+            outputStream.write(header);
+
+            while (inputStream.read(sectorInput) != -1) {
+                int acum = 0;
+                for(int i=0;i<128;i++)
+                {
+                    sectorOutputC[i]=sectorInput[i];
+                    acum += (int)sectorInput[i];
+                    if(acum > 0xFF)
+                    {
+                        acum-=0x100;
+                        acum++;
+                    }
+                }
+                sectorOutputC[128]=(byte)acum;
+               outputStream.write(sectorOutputC);
+            }
+        } catch (Exception e) {
+        }
+
     }
-    
+
 }
